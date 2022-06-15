0b23676470d9f53b76588a579011d2dd847b60d2
jumiej <akinyodejummy177@gmail.com>
2022-06-15 01:14:00 +0100

    adjusment

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// Dom
+// document.getElementById("paral");
+let paral = document.getElementById("paral");
+
+paral.innerHTML = "Hello World";
+paral.style.color = "red";
+paral.style.fontSize = "40px";
+
+let newP = document.createElement("p")
+innerHTML = "Hello Class This is a new para";
+
+document.querySelector("#para3").innerHTML = "Hello class! This is para 3";  
